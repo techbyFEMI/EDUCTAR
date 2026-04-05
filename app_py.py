45c32ae47ffa2e7412e4792c6a3c07e97fb1b897
Femi <techbyfemi@gmail.com>
2026-04-05 01:36:09 +0100

    bug fixed

--- app_py.py
+++ app_py.py
@@ -9,7 +9,6 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
-from numpy import block
 from sqlalchemy.dialects.postgresql import insert
 import pymupdf4llm
 from openai import AsyncOpenAI
